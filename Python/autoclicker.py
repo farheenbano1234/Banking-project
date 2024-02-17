125dc379f8dca4a8bc2ee52b54458c1de3c311d6
farheenbano1234 <114060267+farheenbano1234@users.noreply.github.com>
2024-02-17 14:58:17 +0530

    Delete Python directory

--- Python/autoclicker.py
+++
@@ -1,7 +0,0 @@
-import time
-import threading
-from pynput.mouse import Button, Controller
-  
-# pynput.keyboard is used to watch events of
-# keyboard for start and stop of auto-clicker
-from pynput.keyboard import Listener, KeyCode
